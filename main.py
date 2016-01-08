4f380ac1aacad3aebc0ce787b6d1d75ba17463a8
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-01-08 12:35:09 +0100

    Now with multiprocessing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 Reproduce figure 6 from Steuer et al., (2003)
 """
 
+import multiprocessing
+
 import numpy as np
 from tqdm import tqdm
 
@@ -27,7 +29,7 @@ def analyze_system(system, repetition_num=100, filter_trivial_ss=True):
     plot_ss_scatter(np.array(ss_data))
     corr_mat = compute_correlation_matrix(np.array(ss_data))
 
-    return corr_mat, sol
+    return system, corr_mat, sol
 
 def cluster_data(data):
     """ Order data according to correlation matrices
@@ -39,12 +41,16 @@ def main():
     """
     systems = generate_systems()
 
+    core_num = int(multiprocessing.cpu_count()/2)
+    print('Using %d cores' % core_num)
+
     data = []
-    for syst in tqdm(systems):
-        res = analyze_system(syst)
-        if not res is None:
-            cmat, sol = res
-            data.append((syst, cmat, sol))
+    with tqdm(total=len(systems)) as pbar:
+        with multiprocessing.Pool(core_num) as p:
+            for res in p.imap_unordered(analyze_system, systems, chunksize=10):
+                if not res is None:
+                    data.append(res)
+                pbar.update()
 
     cache_data(data)
     data = cluster_data(data)
