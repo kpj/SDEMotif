0836c812b6e89457e72a3ad68f7c9e2a71caa42d
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-03-03 11:33:22 +0100

    Handle IndexError

--- filters.py
+++ filters.py
@@ -11,7 +11,11 @@ def filter_steady_state(ss, filter_mask, min_thres=1e-10, max_thres=20):
     """
     mask = np.ones(ss.shape, dtype=bool)
     if not filter_mask is None:
-        mask[filter_mask] = 0
+        for ind in filter_mask:
+            try:
+                mask[ind] = 0
+            except IndexError:
+                pass
 
     fss = ss[mask]
     return any(fss < min_thres) or any(fss > max_thres)
