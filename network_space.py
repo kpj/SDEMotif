63a10583048ce2b14092916556f1aed5f979b409
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-05-04 14:34:53 +0200

    Tweak investigation

--- network_space.py
+++ network_space.py
@@ -57,92 +57,16 @@ def bin_entries(row, threshold=0.2):
         'enh_vals': rm(row.enh_vals)
     })
 
-def check_vanishing(row):
-    """ Estimate number of vanishing correlations
-    """
-    mask = np.zeros(row.raw_vals.shape, dtype=bool)
-    nv_inds = np.intersect1d(np.nonzero(row.raw_vals), np.nonzero(row.enh_vals))
-    mask[nv_inds] = 1
-
-    return pd.Series({
-        'raw_res': row.raw_res,
-        'enh_res': row.enh_res,
-        'already_zero': np.sum(row.raw_vals == 0),
-        'abs_corr_diff': np.mean(abs(row.raw_vals[mask] - row.enh_vals[mask]))
-            if len(nv_inds) > 0 else np.nan
-    })
-
-def check_neg_pos_corrs(row):
-    neg_pos = []
-    pos_neg = []
-
-    for raw, enh in zip(row.raw_vals, row.enh_vals):
-        if raw < 0:
-            neg_pos.append(abs(raw-enh))
-        else:
-            pos_neg.append(abs(raw-enh))
-
-    return pd.Series({
-        'raw_res': row.raw_res,
-        'enh_res': row.enh_res,
-        'mean_negpos_corr': np.mean(neg_pos),
-        'mean_posneg_corr': np.mean(pos_neg)
-    })
-
 def check_disruption(row):
+
+    sdiff = (np.sign(row.raw_vals) - np.sign(row.enh_vals)) * abs(row.raw_vals - row.enh_vals)
+
     return pd.Series({
         'raw_res': row.raw_res,
         'enh_res': row.enh_res,
-        'non_trivial_disruption': 1 - np.mean(abs(row.enh_vals)),
-        'mean_disruption': np.mean(abs(row.raw_vals - row.enh_vals))
+        'sign_diff': np.mean(sdiff),
+        'mean_difference': np.mean(abs(row.raw_vals - row.enh_vals))
     })
-
-def plot_zeros_vs_corr_diff(df):
-    """ Plot number of zero correlations in original network against mean of absolute correlation differences
-    """
-    df = df.apply(check_vanishing, axis=1)
-
-    # generate overview
-    plt.figure()
-
-    sns.regplot(
-        x='already_zero', y='abs_corr_diff', data=df,
-        fit_reg=False,
-        x_jitter=0.1)#, y_jitter=0.1)
-
-    save_figure('images/network_space.pdf')
-
-    # plot networks in detail
-    extr = df[df.abs_corr_diff > 0.4]
-    print(extr)
-
-    if not extr.empty:
-        mpl.style.use('default')
-        plot_individuals(
-            list(zip(extr.raw_res.tolist(), extr.enh_res.tolist())),
-            'images/extreme.pdf')
-
-def plot_neg_pos_corrs(df):
-    """ Plot neg - pos corrs on x and pos - neg corrs on y axis
-    """
-    df = df.apply(check_neg_pos_corrs, axis=1)
-
-    # generate overview
-    plt.figure()
-    sns.regplot(
-        x='mean_negpos_corr', y='mean_posneg_corr', data=df,
-        fit_reg=False)
-    save_figure('images/network_space2.pdf')
-
-    # plot networks in detail
-    extr = df[(df.mean_negpos_corr > 0.6) & (df.mean_posneg_corr > 0.6)]
-    print(extr)
-
-    if not extr.empty:
-        mpl.style.use('default')
-        plot_individuals(
-            list(zip(extr.raw_res.tolist(), extr.enh_res.tolist()))[:5],
-            'images/extreme2.pdf')
 
 def plot_disruptiveness(df):
     """ Compare disruption of correlations (mean difference) to trivial disruption (1 mean(corr. of 4node network))
@@ -152,19 +76,19 @@ def plot_disruptiveness(df):
     # generate overview
     plt.figure()
     sns.regplot(
-        x='non_trivial_disruption', y='mean_disruption', data=df,
+        x='sign_diff', y='mean_difference', data=df,
         fit_reg=False)
-    save_figure('images/network_space2.pdf')
+    save_figure('images/network_space.pdf')
 
     # plot networks in detail
-    extr = df[(df.non_trivial_disruption >= 0.6) & (df.mean_disruption >= 0.3)]
+    extr = df[(df.sign_diff >= 0.3) & (df.mean_difference >= 0.4)]
     print(extr)
 
     if not extr.empty:
         mpl.style.use('default')
         plot_individuals(
             list(zip(extr.raw_res.tolist(), extr.enh_res.tolist())),
-            'images/extreme2.pdf')
+            'images/extreme.pdf')
 
 def main(data):
     """ Analyse data
@@ -174,8 +98,6 @@ def main(data):
         lambda row: bin_entries(extract_entries(row)),
         axis=1)
 
-    #plot_neg_pos_corrs(df)
-    #plot_zeros_vs_corr_diff(df)
     plot_disruptiveness(df)
 
 
