1ff5563076459f1cf341b50a60bfa7876077b5ee
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2016-12-09 23:03:45 +0100

    Add simple data interpretation

--- pipeline.py
+++ pipeline.py
@@ -3,16 +3,24 @@ Clean pipeline of robustness detection
 """
 
 import sys
+import copy
 import pickle
-from typing import Tuple, List
+from typing import Tuple, List, Callable
+from multiprocessing import Pool, cpu_count
 
 import numpy as np
 import pandas as pd
+import scipy.stats as scis
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from tqdm import tqdm
+from tqdm import tqdm, trange
+
+from solver import solve_system
+from filters import filter_steady_state
+from setup import generate_basic_system
+from nm_data_generator import add_node_to_system
 
 
 def threshold_div(distr: np.ndarray, thres: float) -> Tuple[np.ndarray, np.ndarray]:
@@ -63,12 +71,103 @@ def initial_tests(n: int = 1000) -> None:
 
     plt.savefig('images/robustness_development.pdf')
 
-def handle_enh_entry(raw, enh, thres: float) -> float:
+def simulate_systems(raw, enhanced, reps=100):
+    """ Simulate given systems and return raw vs enhanced versions
+    """
+    def sim(sde_system):
+        ode_system = copy.copy(sde_system)
+        ode_system.fluctuation_vector = np.zeros(sde_system.fluctuation_vector.shape)
+
+        corr_mats = []
+        for _ in trange(reps):
+            sde_sol = solve_system(sde_system)
+            ode_sol = solve_system(ode_system)
+
+            sol = ode_sol - sde_sol
+            sol_extract = sol.T[int(len(sol.T)*3/4):] # extract steady-state
+
+            if filter_steady_state(ode_sol.T[int(len(ode_sol.T)*3/4):]):
+                continue
+
+            # compute correlations
+            dim = sol_extract.shape[1]
+            mat = np.empty((dim,dim))
+            for i in range(dim):
+                for j in range(dim):
+                    xs, ys = sol_extract[:,i], sol_extract[:,j]
+                    cc, pval = scis.pearsonr(xs, ys)
+                    mat[i,j] = cc
+
+            corr_mats.append(mat)
+        return np.asarray(corr_mats)
+
+    curs = []
+    for enh in tqdm(enhanced):
+        curs.append(sim(enh))
+
+    return {
+        'raw_corr_mats': sim(raw),
+        'enh_corr_mat_list': curs #[sim(enh) for enh in enhanced]
+    }
+
+def generate_data(
+    fname: str, gen_func: Callable = generate_basic_system,
+    paramter_shift: int = 10
+) -> None:
+    """ Generate individual correlation realizations for varying parameters and embeddings
+    """
+    param_range = np.linspace(0.1, 5, paramter_shift)
+
+    # generate data
+    configurations = []
+    for k_m in param_range:
+        for k_23 in param_range:
+            syst = gen_func(k_m=k_m, k_23=k_23)
+            more = add_node_to_system(syst)
+            configurations.append((syst, more))
+
+    # simulate data
+    data = []
+    with tqdm(total=len(configurations)) as pbar:
+        resolution = int(cpu_count() * 3/4)
+        with Pool(resolution) as p:
+            for res in p.starmap(simulate_systems, configurations):
+                if not res is None:
+                    data.append(res)
+                pbar.update()
+
+    # store data
+    with open(fname, 'wb') as fd:
+        pickle.dump({
+            'data': data
+        }, fd)
+
+def handle_enh_entry(entry, thres: float) -> float:
     """ Compare distribution from given simulation results
     """
-    return 1
+    raw_corr_mats = entry['raw_corr_mats']
+    enh_corr_mat_list = entry['enh_corr_mat_list']
 
-def threshold_influence(data: List, resolution: int = 5) -> None:
+    cur = []
+    skipped = 0
+    coords = [(0,1), (0,2), (1,2)]
+    for enh_corr_mats in enh_corr_mat_list:
+        if enh_corr_mats.size == 0:
+            skipped += 1
+            continue
+
+        for i,j in coords:
+            dis_3 = raw_corr_mats[:,i,j]
+            dis_4 = enh_corr_mats[:,i,j]
+            rob = compare_distributions(dis_3, dis_4, thres)
+            cur.append(rob)
+
+    #if skipped > 0:
+    #    print('Skipped {} enhanced correlation matrices'.format(skipped))
+
+    return np.sum(cur)
+
+def threshold_influence(data: List, resolution: int = 500) -> None:
     """ Plot robustness for varying threshold levels
     """
     threshold_list = np.logspace(-5, 0, resolution-1)
@@ -77,8 +176,8 @@ def threshold_influence(data: List, resolution: int = 5) -> None:
     df = pd.DataFrame()
     for thres in tqdm(threshold_list):
         cur = []
-        for raw, enh_res in data:
-            res = [handle_enh_entry(raw, enh, thres) for enh in enh_res]
+        for entry in data:
+            res = handle_enh_entry(entry, thres)
             cur.append(res)
         df = df.append({
             'threshold': thres,
@@ -106,8 +205,8 @@ if __name__ == '__main__':
     plt.style.use('seaborn-poster')
 
     if len(sys.argv) == 1:
-        print('No arguments given, running tests')
-        initial_tests()
+        #initial_tests()
+        generate_data('results/new_data.dat')
     elif len(sys.argv) == 2:
         main(sys.argv[1])
     else:
