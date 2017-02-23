24b55cfacd8644de18bd571db4c8316ef9ed0c44
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-02-23 11:49:24 +0100

    Improve embedded plot

--- larger_embedding.py
+++ larger_embedding.py
@@ -48,30 +48,25 @@ def get_system(N, v_in=5, D=1):
     #system.save('cache/embedded_system.pkl')
     return system
 
-def plot_solution(syst, sol):
-    plt.figure(figsize=(20, 6))
-    gs = gridspec.GridSpec(1, 2, width_ratios=[2,4])
-
-    ax = plt.subplot(gs[0])
+def plot_system(syst, ax):
     graph = nx.from_numpy_matrix(syst.jacobian.T, create_using=nx.DiGraph())
     nx.draw(
-        graph, ax=ax,
-        with_labels=True)
+        graph, ax=ax, with_labels=True,
+        node_color=['y' if n in range(3) else 'red' for n in graph.nodes()])
 
-    ax = plt.subplot(gs[1])
+def plot_solution(sol, ax):
     for i, series in enumerate(sol):
         ax.plot(series, label=i, alpha=1 if i in range(3) else .3)
     ax.legend(loc='best')
 
-    plt.tight_layout()
-    plt.savefig('images/embedded_motif.pdf')
-
-def plot_correlation_hist(matrices, label, color):
+def plot_correlation_hist(matrices, label, color, ax):
     for i, row in enumerate(matrices.T):
         for j, series in enumerate(row):
             if i == j: break
-            sns.distplot(series, label=label if i == 1 else None, color=color, hist=False)
-    plt.legend(loc='best')
+            sns.distplot(
+                series, ax=ax, hist=False,
+                label=label if i == 1 else None, color=color)
+    ax.legend(loc='best')
 
 def simulate(syst, reps=1000):
     matrices = []
@@ -83,28 +78,37 @@ def simulate(syst, reps=1000):
             pbar.update()
             reps -= 1
 
-            if reps == 0:
-                plot_solution(syst, sol)
-
             x,y = mat.shape
             if mat.shape != (3,3):
                 mat = mat[:-(x-3), :-(y-3)]
             assert mat.shape == (3, 3), mat.shape
 
             matrices.append(mat)
-    return np.asarray(matrices)
+    return np.asarray(matrices), (syst, sol)
 
 def main():
+    # compute data
     bas_syst = generate_basic_system()
     emb_syst = SDESystem.load('cache/embedded_system.pkl') #get_system(10)
 
-    bas_mats = simulate(bas_syst)
-    emb_mats = simulate(emb_syst)
+    bas_mats, bas_extra = simulate(bas_syst, 30)
+    emb_mats, emb_extra = simulate(emb_syst, 30)
 
-    plt.figure()
-    plot_correlation_hist(bas_mats, 'only motif', 'blue')
-    plot_correlation_hist(emb_mats, 'embedded motif', 'red')
-    plt.savefig('images/embedded_motif_corr_hist')
+    # generate plot
+    plt.figure(figsize=(20, 6))
+    gs = gridspec.GridSpec(2, 3, width_ratios=[2,4,2])
+
+    plot_system(bas_extra[0], plt.subplot(gs[0,0]))
+    plot_system(emb_extra[0], plt.subplot(gs[1,0]))
+
+    plot_solution(bas_extra[1], plt.subplot(gs[0,1]))
+    plot_solution(emb_extra[1], plt.subplot(gs[1,1]))
+
+    plot_correlation_hist(bas_mats, 'only motif', 'blue', plt.subplot(gs[0,2]))
+    plot_correlation_hist(emb_mats, 'embedded motif', 'red', plt.subplot(gs[1,2]))
+
+    plt.tight_layout()
+    plt.savefig('images/embedded_motif.pdf')
 
 
 if __name__ == '__main__':
