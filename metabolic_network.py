42ae5e1f9a2150bde33e43ed4a8d8ed914840adc
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-05-14 13:06:56 +0200

    Force different reaction ids

--- metabolic_network.py
+++ metabolic_network.py
@@ -1,8 +1,26 @@
+import itertools
+
 import cobra
 from cameo.network_analysis.networkx_based import model_to_network
 
 from reaction_finder import detect_ffl
 
+
+def reaction_ids_unique(cs, graph):
+    """ Check that unique reactions are used for FFL
+    """
+    d1 = graph.get_edge_data(cs[0], cs[1])
+    d2 = graph.get_edge_data(cs[0], cs[2])
+    d3 = graph.get_edge_data(cs[1], cs[2])
+    assert graph.get_edge_data(cs[1], cs[0]) is None
+    assert graph.get_edge_data(cs[2], cs[0]) is None
+    assert graph.get_edge_data(cs[2], cs[1]) is None
+
+    for r_group in itertools.product(d1.values(), d2.values(), d3.values()):
+        idx = tuple(map(lambda x: x['reaction'].id, r_group))
+        if len(set(idx)) < 3:
+            return False
+    return True
 
 def main():
     # load model
@@ -12,8 +30,11 @@ def main():
     # extract motifs
     motifs = []
     for cs in detect_ffl(graph):
-        if None in cs: continue
-        if cs in motifs: continue
+        if None in cs or cs in motifs:
+            continue
+        if not reaction_ids_unique(cs, graph):
+            continue
+
         motifs.append(cs)
     motifs_n = [tuple(map(lambda x: x.name, mot)) for mot in motifs]
 
@@ -21,16 +42,18 @@ def main():
     marker = [
         'cofactors', 'pool',
         'Na', 'Pi', 'CoA', 'apo',
-        'H2O', 'O2_O2', 'CO2_CO2', 'H+_H',
+        'H2O', 'O2_O2', 'CO2_CO2', 'H+_H', 'HCO3-',
         'AMP', 'ADP', 'ATP', 'UMP', 'UDP', 'UTP', 'GMP', 'GDP', 'GTP',
         'NAD', 'NAD'
     ]
-    match = lambda x: all(not w in x for w in marker)
+    match = lambda x: all(w not in x for w in marker)
 
     with open('results/metabolite_list.txt', 'w') as fd:
         for m in motifs_n:
-            if not all(map(match, m)): continue
+            if not all(map(match, m)):
+                continue
             fd.write(f'{m}\n')
+
 
 if __name__ == '__main__':
     main()
