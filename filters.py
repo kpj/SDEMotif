6f7fdda3bb61bda4baefaf7e882eeecea7c6c6f1
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-06-25 02:06:29 +0200

    Undo breaking filter changes

--- filters.py
+++ filters.py
@@ -6,7 +6,7 @@ An entry is filtered if a filter returns True
 import numpy as np
 
 
-def filter_steady_state(ss, filter_mask=None, min_thres=1e-10, min_inc=1e-1):
+def filter_steady_state(ss, filter_mask=None, min_thres=1e-10, min_inc=1):
     """ Discard steady states which are trivial (~= `min_thres`) or diverge (differences increase by at least `min_inc`)
     """
     fss = np.copy(ss)
@@ -20,7 +20,7 @@ def filter_steady_state(ss, filter_mask=None, min_thres=1e-10, min_inc=1e-1):
     # check divergence
     old_diff = abs(fss[1] - fss[0])
     new_diff = abs(fss[-1] - fss[-2])
-    inc_diff = new_diff >= old_diff
+    inc_diff = new_diff > old_diff
     div_res = inc_diff.any() and (new_diff > min_inc).any()
 
     # check convergence to 0
