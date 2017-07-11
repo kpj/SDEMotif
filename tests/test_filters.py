bd2968cb3c662cfa9791297671b320e0d7a5f437
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-07-11 18:57:49 +0200

    Fix test

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -22,7 +22,7 @@ class TestSteadyStateFilter(TestCase):
         ])
 
         res = filter_steady_state(ss, [1])
-        self.assertTrue(res)
+        self.assertFalse(res)
 
     def test_divergence(self):
         ss = np.array([
