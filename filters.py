846ac307bd41be9af368c56a91564b57d4062ee9
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-07-11 16:20:25 +0200

    Small filter change

--- filters.py
+++ filters.py
@@ -6,8 +6,8 @@ An entry is filtered if a filter returns True
 import numpy as np
 
 
-def filter_steady_state(ss, filter_mask=None, min_thres=1e-10, min_inc=1):
-    """ Discard steady states which are trivial (~= `min_thres`) or diverge (differences increase by at least `min_inc`)
+def filter_steady_state(ss, filter_mask=None, min_thres=1e-10):
+    """ Discard steady states which are trivial (~= `min_thres`) or diverge
     """
     fss = np.copy(ss)
     if not filter_mask is None:
@@ -21,7 +21,7 @@ def filter_steady_state(ss, filter_mask=None, min_thres=1e-10, min_inc=1):
     old_diff = abs(fss[1] - fss[0])
     new_diff = abs(fss[-1] - fss[-2])
     inc_diff = new_diff > old_diff
-    div_res = inc_diff.any() and (new_diff > min_inc).any()
+    div_res = inc_diff.any()
 
     # check convergence to 0
     conv_res = (fss < min_thres).any()
